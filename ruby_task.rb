5a97ec0910249cf47b278e31a666250d6606e637
武田　佑美香 <yumikatakeda@takedayumikanoMacBook-Pro.local>
2020-02-18 20:05:44 +0900

    first commit

---
+++ ruby_task.rb
@@ -0,0 +1,18 @@
+#Q1
+
+names = ["田中","佐藤","佐々木","高橋"]
+
+p names
+
+names.push ("斎藤")
+
+p names
+
+#Q2
+
+array1 = %w(dog cat fish)
+array2 = %w(bird bat tiger)
+
+array = array1 + array2
+
+p array
